2d6f44449ab2af32973cbb2435453787727b5b7d
kangju <kangju@kangju>
2017-11-08 13:40:58 +0900

    2017.11.08.13:40

--- jsmooth/sample/src/JSmoothPropertiesDisplayer.java
+++ jsmooth/sample/src/JSmoothPropertiesDisplayer.java
@@ -29,11 +29,11 @@ public class JSmoothPropertiesDisplayer extends java.awt.Frame
 	 */
     private void initComponents() {//GEN-BEGIN:initComponents
         java.awt.GridBagConstraints gridBagConstraints;
-
+/*
         label1 = new java.awt.Label();
         m_text = new java.awt.TextArea();
         m_buttonClose = new java.awt.Button();
-
+*/
         setLayout(new java.awt.GridBagLayout());
 
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -41,11 +41,11 @@ public class JSmoothPropertiesDisplayer extends java.awt.Frame
                 exitForm(evt);
             }
         });
-/*
+
         label1.setAlignment(java.awt.Label.CENTER);
         label1.setFont(new java.awt.Font("Dialog", 0, 18));
         label1.setText("JSmooth Sample Program");
-        */
+        
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
